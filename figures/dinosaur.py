9dc2015fe34cbe648e652e6c8534d25a4f5da26f
Ladina Morf <ladina.morf@students.ffhs.ch>
2025-11-08 10:53:33 +0100

    i guess better than it was not too long ago

--- figures/dinosaur.py
+++ figures/dinosaur.py
@@ -4,8 +4,8 @@ class Dinosaur:
     X_POS = 80
     Y_POS = 310
     Y_POS_DUCK = 340
-    JUMP_VEL = 12.0    
-    GRAVITY  = 1.2  # constant gravity per frame
+    JUMP_VEL = 8.5    
+    GRAVITY  = 0.8  # retained for reference; classic jump uses fixed decrement
 
     def __init__(self, run_img, jump_img, duck_img):
         # Sprites / assets
@@ -103,14 +103,17 @@ class Dinosaur:
 
     def jump_anim(self):
         self.image = self.jump_img
-        self.dino_rect.y -= self.jump_vel
-        self.jump_vel -= self.GRAVITY
-        if self.dino_rect.y >= self.Y_POS:
+        if self.dino_jump:
+            # Classic, snappy Chrome-Dino jump: fast ascent, smooth descent
+            self.dino_rect.y -= self.jump_vel * 4
+            self.jump_vel -= 0.8
+        if self.jump_vel < -self.JUMP_VEL:
+            # Land
             self.dino_rect.y = self.Y_POS
-            self.dino_jump = False
-            self.dino_duck = False
             self.dino_run  = True
-            self.jump_vel  = 0.0
+            self.dino_duck = False
+            self.dino_jump = False
+            self.jump_vel  = self.JUMP_VEL
 
     def draw(self, SCREEN):
         SCREEN.blit(self.image, (self.dino_rect.x, self.dino_rect.y))
